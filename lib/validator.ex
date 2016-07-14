ca90d44ce0acd4ec5a7a6f93263a509a38d27077
Chris Steinmeyer <csteinmeyer@gmail.com>
2016-07-14 12:01:19 -0700

    validate uuids

--- lib/validator.ex
+++ lib/validator.ex
@@ -1,2 +1,9 @@
 defmodule Validator do
+  @uuid ~r/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+
+  def validate_uuid(value), do: test(value, @uuid)
+
+  defp test(value, @uuid) do
+    Regex.match?(@uuid, value)
+  end
 end
--- test/validator_test.exs
+++ test/validator_test.exs
@@ -2,7 +2,13 @@ defmodule ValidatorTest do
   use ExUnit.Case
   doctest Validator
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  ####  UUIDS
+  test "a valid UUID" do
+    assert Validator.validate_uuid("d03d503d-d99e-48c9-8fc5-e227b57cf90f")
   end
+
+  test "an invalid UUID" do
+    assert Validator.validate_uuid("chrisfishwood") != true
+  end
+
 end
